b2c9c70200434bfb2160940ae2da3ba4462bd8d6
Emr03 <elsa.riachi@mail.mcgill.ca>
2015-11-02 21:49:42 -0500

    Draft version

---
+++ ColorSensorPoller.java
@@ -0,0 +1,11 @@
+package Project;
+
+import lejos.hardware.sensor.EV3ColorSensor;
+
+public class ColorSensorPoller {
+
+	public ColorSensorPoller(EV3ColorSensor colorsensor) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
