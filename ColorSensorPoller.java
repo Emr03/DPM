fa0a246f05927233d74de05bd767e5fa90588dd2
amambo <alina.mambo@gmail.com>
2015-11-03 16:51:41 -0500

    add fields to ColorSensorPoller

--- ColorSensorPoller.java
+++ ColorSensorPoller.java
@@ -4,8 +4,15 @@ import lejos.hardware.sensor.EV3ColorSensor;
 
 public class ColorSensorPoller {
 
+	private SampleProvider colorValue;
+	private float[] colorData;
+	public int colorID;
+	
+	
+	
 	public ColorSensorPoller(EV3ColorSensor colorsensor) {
 		// TODO Auto-generated constructor stub
+		
 	}
 
 }
