9ce765d91b16308a93c61af00ae7e3a421ec6004
amambo <alina.mambo@gmail.com>
2015-11-03 21:29:57 -0500

    Change localization constructor

--- Localization.java
+++ Localization.java
@@ -1,5 +1,46 @@
 package Project;
 
-public class Localization {
+import lejos.hardware.lcd.TextLCD;
+import lejos.robotics.SampleProvider;
+import localization.Navigation;
+import localization.Odometer;
+import localization.USLocalizer.LocalizationType;
+
+public static double ROTATION_SPEED = 30;
+
+private Odometer odo;
+private SampleProvider usSensor;
+private float[] usData;
+private int corner;
+private Navigation navigate;
+private UltrasonicPoller usPoller_left;
+private UltrasonicPoller usPoller_right;
+
+
+private final int maxDist = 50;
+private final int d = 30;
+private final int k = 7;
+private final int width = 15;
+
+
+public class Localization  extends Thread{
+	
+	public USLocalizer(Odometer odo,Navigation navigate, UltrasonicPoller usPoller_left, UltrasonicPoller usPoller_right) {
+		this.odo = odo;
+		this.usSensor = usSensor;
+		this.usData = usData;
+		this.navigate=navigate;
+		this.usPoller_left=usPoller_left;
+		this.usPoller_right=usPoller_right;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
