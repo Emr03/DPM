d3b2383e3b31fb7cef06d6ca0d217d56b24e3aa9
amambo <alina.mambo@gmail.com>
2015-11-07 18:08:07 -0500

    Update UltrasonicPoller.java

--- src/Project/UltrasonicPoller.java
+++ src/Project/UltrasonicPoller.java
@@ -2,12 +2,33 @@ package Project;
 
 import lejos.robotics.SampleProvider;
 
+/**
+ * Ultrasonic Poller Class
+ * This class polls the ultrasonic sensor for distances.
+ * This class is implemented as a thread.
+ * 
+ * @author DPM TEAM 18
+ * @version 1.0, 7 Nov 2015
+ * 
+ *
+ */
+
+
 public class UltrasonicPoller extends Thread {
+	
+	
 	private SampleProvider us;
 	private float[] usData;
 	public int distance;
 	private Object lock = new Object();
+	
 
+	/**
+	 * Creates an Object of type UltrasonicPoller
+	 * Creates and assigns an array of floats used to store the distance values.
+	 * @param us is a SampleProvider
+	 * 
+	 */
 	public UltrasonicPoller(SampleProvider us) {
 		this.us = us;
 		this.usData = new float[us.sampleSize()];
@@ -16,6 +37,14 @@ public class UltrasonicPoller extends Thread {
 	// Sensors now return floats using a uniform protocol.
 	// Need to convert US result to an integer [0,255]
 
+	/**
+	 * This method is called when the thread is started 
+	 * It acquires data from the sensor.
+	 * 
+	 * 
+	 */
+	
+	
 	public void run() {
 		while (true) {
 			us.fetchSample(usData, 0); // acquire data
@@ -29,6 +58,11 @@ public class UltrasonicPoller extends Thread {
 		}
 	}
 
+	
+	/**
+	 * This method updates the distances recognized by the UltraSonic sensor.
+	 * @return int assigned the value of the distance seen by the US sensor.
+	 */
 	public int getDistance() {
 		int result = this.distance; 
 		synchronized (lock){
@@ -36,10 +70,5 @@ public class UltrasonicPoller extends Thread {
 		}
 	}
 	
-	//****NEED TO ADD****///
-	//filter method (cap + median?)
-	//if distance falls below critical value and robot state 
-	//interrupt navigation thread 
-	
-}
 
+}
