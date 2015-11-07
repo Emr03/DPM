2638cb8ee2d6a9a95f3d46058b8e7aca3094b388
amambo <alina.mambo@gmail.com>
2015-11-07 18:06:14 -0500

    Update ColorSensorPoller

--- src/Project/ColorSensorPoller.java
+++ src/Project/ColorSensorPoller.java
@@ -1,18 +1,71 @@
 package Project;
 
 import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.robotics.SampleProvider;
 
-public class ColorSensorPoller {
+/**
+ * ColorSensorPoller Class
+ * This class polls the ColorSensor sensor for colorIDs.
+ * This class is implemented as a thread.
+ * 
+ * 
+ * @author DPM TEAM18
+ * @version 1.0, 7 Nov 2015
+ * 
+ *
+ */
+
+
+
+
+public class ColorSensorPoller extends Thread{
 
 	private SampleProvider colorValue;
 	private float[] colorData;
-	public int colorID;
+	public float colorID;
+	private Object lock;
 	
 	
-	
+	/**
+	 * Creates an Object of type ColorSensorPoller
+	 * The constructor populates a SampleProvider and an array floats with ColorID data
+	 * @param colorsensor is an EV3ColorSensor
+	 */
 	public ColorSensorPoller(EV3ColorSensor colorsensor) {
-		// TODO Auto-generated constructor stub
+		this.colorValue=colorsensor.getMode("ColorID");
+		this.colorData = new float[colorValue.sampleSize()];
 		
 	}
+	
+	
+	/**
+	 * This method is called when the thread is started 
+	 * It acquires data from the sensor.
+	 */
+	public void run(){
+		while(true){
+			colorValue.fetchSample(colorData,0);
+			synchronized(lock){
+				this.colorID=(colorData[0]);
+			}
+			try {
+				Thread.sleep(50);
+			} catch (Exception e) {
+			} // Poor man's timed sampling
+		}
+	}
+	
+	/**
+	 * This method returns the ColorID 
+	 * @return float
+	 */
+	
+	public float getColor(){
+		float result= this.colorID;
+		synchronized(lock){
+			return result;
+		}
+	}
+	
 
 }
